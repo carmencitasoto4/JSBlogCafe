07cc331a3d626c5056eb41b53af14e1ba974d3b7
carmencitasoto410 <csoto@sfvc.edu.ar>
2024-10-24 18:51:00 -0300

    Clase del 23/10

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
-console.log("Hola mundo")
+/* console.log("Hola")
+
+let nombre = "Nodo Tecnologico";
+
+let edad = 18 
+
+console.log("Hola soy " + nombre + "y tengo" + edad + "años")
+
+// Definicion de una función 
+function suma(numero1 , numero2){
+    return numero1 + numero2
+}
+
+//Llamado a la función
+
+let resultado = suma("5","8")
+
+console.log(resultado) */
+
+const persona = {
+   nombre: "Nodo Tecnologico",
+   edad: 5,
+   anioDeNacimiento: 2020
+   
+}
+
+console.log(" hola soy el " + persona.edad)
+
+
+const textoHeader = document.querySelector('#heading')
+
+console.log(textoHeader)
+
+textoHeader.textContent = "Nuevo Texto"
+
+textoHeader.classList.add('nueva-clase')
+
+const enlaces= document.querySelectorAll('.navegacion a')
+
+console.log(enlaces[0])
+
+enlaces[1].textContent= "Nuevo enlace"
